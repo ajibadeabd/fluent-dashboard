ab060943c156604898c58d6866f494fb8a915deb
championshuttler <shivams2799@gmail.com>
2020-06-14 08:39:41 -0400

    Update the name to fluentbit dashboard

--- vue.config.js
+++ vue.config.js
@@ -30,7 +30,7 @@ module.exports = {
       filename: 'index.html',
       // when using title option,
       // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
-      title: 'Vuestic Admin',
+      title: 'Fluentbit Dashboard',
       // chunks to include on this page, by default includes
       // extracted common chunks and vendor chunks.
       chunks: ['chunk-vendors', 'chunk-common', 'index'],
